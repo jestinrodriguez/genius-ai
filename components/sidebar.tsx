cabd43ab572aada2ca7f43859c1a668d7e57ce50
Jestin Rodriguez <40747173+jestinrodriguez@users.noreply.github.com>
2023-12-14 21:52:01 -0800

    finished limit UI counter

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -8,6 +8,7 @@ import { Montserrat } from 'next/font/google'
 import { cn } from '@/lib/utils'
 import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from 'lucide-react'
 import { usePathname } from 'next/navigation'
+import FreeCounter from '@/components/free-counter'
 
 const montserrat = Montserrat({
     weight: "600", subsets: ["latin"]
@@ -55,9 +56,13 @@ const routes =[
         icon: Settings,
         href: "/settings",
     },
-]
+];
 
-const Sidebar = () => {
+interface SidebarProps {
+    apiLimitCount: number;
+}
+
+const Sidebar = ({apiLimitCount = 0} : SidebarProps) => {
     const pathname = usePathname();
   return (
     <div className='space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white'>
@@ -90,6 +95,7 @@ const Sidebar = () => {
                 ))}
             </div>
         </div>
+        <FreeCounter apiLimitCount={apiLimitCount} />
     </div>
   )
 }
